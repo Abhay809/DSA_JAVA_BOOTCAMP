2a8e55338866531fcaeb700311c4c7b96ab9511a
yahbamittal7976933120 <123982720+yahbamittal7976933120@users.noreply.github.com>
2024-07-14 22:53:19 +0530

    Sorting Algorithms

--- 9_Strings/src/a3_Output.java
+++ 9_Strings/src/a3_Output.java
@@ -1,6 +1,8 @@
 public class a3_Output {
     public static void main(String[] args) {
         System.out.println(56);  // printing with the help of two string method
+        Integer num = new Integer(56);  // rapper class
+        System.out.println(num.toString());
         System.out.println("Abhay");
         System.out.println(new int[]{2,3,4,5,6}); // new int[]{2,3,4,5,6}, this is object, which can be of any type
 
---
+++ 9_Strings/src/prettyprinting.java
@@ -0,0 +1,5 @@
+public class prettyprinting {
+    public static void main(String[] args) {
+        
+    }
+}
